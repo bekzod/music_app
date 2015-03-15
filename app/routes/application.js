0e3b97ef3ca6d2f504071f0747827a3f714682a4
bek <bekzod@me.com>
2015-03-15 21:16:19 +0300

    added nprogress

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,17 +2,26 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   musicApi: Ember.inject.service(),
+
+  actions: {
+    loading: function(){
+      NProgress.start();
+    },
+    didTransition: function(){
+      NProgress.done();
+    },
+    toNextVideo: function(){
+      var currentVideoId = this.modelFor('video').paramId;
+      this.controller.toNextVideo(currentVideoId);
+    },
+    toPreviousVideo: function(){
+      var currentVideoId = this.modelFor('video').paramId;
+      this.controller.toPreviousVideo(currentVideoId);
+    }
+  },
+
   model: function(){
     return this.get('musicApi').getTopTracks();
-  },
-  actions: {
-   toNextVideo: function(){
-    var currentVideoId = this.modelFor('video').paramId;
-    this.controller.toNextVideo(currentVideoId);
-   },
-   toPreviousVideo: function(){
-    var currentVideoId = this.modelFor('video').paramId;
-    this.controller.toPreviousVideo(currentVideoId);
-   },
   }
+
 });
