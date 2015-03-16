10aaef8aadbd8c315a4373f13e19c26ea3ac5dac
bek <bekzod@me.com>
2015-03-16 12:29:49 +0300

    removed deferReadiness from youtube-iframe-api

--- app/initializers/youtube-iframe-api.js
+++ app/initializers/youtube-iframe-api.js
@@ -1,10 +1,8 @@
-export function initialize(container, application) {
+export function initialize() {
   var tag = document.createElement('script');
   tag.src = "https://www.youtube.com/iframe_api";
   var firstScriptTag = document.getElementsByTagName('script')[0];
   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-  application.deferReadiness();
-  window.onYouTubeIframeAPIReady = application.advanceReadiness.bind(application);
 }
 
 export default {
